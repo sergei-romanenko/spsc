b01d2825400e5d2db557399fa84f051b3709d625
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2008-03-10 17:01:18 +0000

    spsc web application prototype

---
+++ spsc_web/src/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,26 @@
+package bootstrap.liftweb
+
+import net.liftweb.util._
+import net.liftweb.http._
+import net.liftweb.sitemap._
+import net.liftweb.sitemap.Loc._
+import Helpers._
+ 
+/**
+  * A class that's instantiated early and run.  It allows the application
+  * to modify lift's environment
+  */
+class Boot {
+  def boot {
+    // where to search snippet
+    LiftServlet.addToPackages("spsc")     
+
+    // Build SiteMap
+    val entries = Menu(Loc("Home", "/", "Home")) :: 
+                  Menu(Loc("spsc1", "/spsc", "SPSC online")) ::
+                  Menu(Loc("spsc2", "/spsc_result", "SPSC result", Hidden)) ::
+                  Nil 
+    LiftServlet.setSiteMap(SiteMap(entries:_*))
+  }
+}
+
---
+++ spsc_web/src/spsc/snippet/SPSC.scala
@@ -0,0 +1,32 @@
+package spsc.snippet;
+
+import net.liftweb.http.S
+import spsc.ProcessTreeSVG._
+import spsc.ResidualProgramGenerator._
+import spsc.SmallLanguage._
+import spsc.SuperCompiler
+import scala.util.parsing.input.CharArrayReader
+
+class SPSC {
+  def input = <pre>{S.param("program").openOr("")}</pre>
+  def output = 
+  
+  {
+    val result = SmallLanguageParsers.parseProgram(new CharArrayReader(S.param("program").openOr("").toCharArray))
+    if (!result.successful){
+      <pre>{result.toString}</pre>
+    } else {
+      val program = new Program(result.get)
+      val fname = S.param("fname").openOr("")
+      program.definitions.find(d => d match {case f:FFunction if f.name == fname => true; case _ => false;}) match {
+        case None => <pre>Error: f-function with name {fname} is not defined.</pre>
+        case Some(f) =>
+          val function = program.getFFunction(fname)
+          val sc = new SuperCompiler(program)
+          val pt = sc.buildProcessTree(FCall(function.name, function.args))
+          <p><pre>{generateResidualProgram(pt).toString}</pre><br/>{treeToSVG(pt)}</p>
+      }      
+    }
+  }
+  def svg = <pre>{S.param("program").openOr("")}</pre>
+}
