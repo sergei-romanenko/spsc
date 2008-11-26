eaeaeb7b6d2d308904ed7f4889920a41c922bdab
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2008-11-26 15:48:50 +0000

    using scala/lift web application as back end for validation+supercompilation

--- spsc_app/spsc/spsc.py
+++ spsc_app/spsc/spsc.py
@@ -3,13 +3,94 @@ import os
 import sys
 import pyparsing
 import slparsing
+import urllib
 
-from google.appengine.ext.webapp import template
+from xml.dom.minidom import parse, parseString
+
+from google.appengine.api import users
+from google.appengine.api import urlfetch
 
 from google.appengine.ext import db
-from google.appengine.api import users
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+
+OK = 'ok'
+UNKNOWN_FUNCTION = 'unknownFunction'
+PARSE_ERROR = 'parseError'
+NETWORK_ERROR = 'networkError'
+
+VALIDATION_URL = 'http://pat.keldysh.ru/spsc_web/validate'
+RUN_URL = 'http://pat.keldysh.ru/spsc_web/run'
+
+class ValidationResult(object):
+    def __init__(self, status, message=None, line=None, column=None, code_line=None):
+        self.status = status
+        self.message = message
+        self.line = line
+        self.column = column
+        self.code_line = code_line
+        
+def validateProgram(code, goal):
+    form_fields = {'program': code, 'fname': goal}
+    form_data = urllib.urlencode(form_fields)
+    result = urlfetch.fetch(url=VALIDATION_URL,
+                            payload=form_data,
+                            method=urlfetch.POST,
+                            headers={'Content-Type': 'application/x-www-form-urlencoded'})   
+    if result.status_code == 200:
+        xmlresponse = result.content
+        dom = parseString(xmlresponse)
+        status = dom.documentElement.getAttribute('status')
+        if status == PARSE_ERROR:
+            details = dom.documentElement.getElementsByTagName('details')[0]
+            msg = details.getAttribute('message')
+            line = int(details.getAttribute('line'))
+            column = int(details.getAttribute('column'))
+            code_line = ''
+            lines = code.splitlines()
+            if lines:
+                code_line = lines[line-1] 
+            return ValidationResult(status, message=msg, line=line, column=column, code_line=code_line)
+        else:
+            return ValidationResult(status)
+    else:
+        return ValidationResult(NETWORK_ERROR)
+
+class SupercompilationResult(object):
+    def __init__(self, status, residualCode=None, svgTree=None):
+        self.status = status
+        self.residualCode = residualCode
+        self.svgTree = svgTree
+        
+def supercompileProgram(code, goal):
+    form_fields = {'program': code, 'fname': goal}
+    form_data = urllib.urlencode(form_fields)
+    result = urlfetch.fetch(url=RUN_URL,
+                            payload=form_data,
+                            method=urlfetch.POST,
+                            headers={'Content-Type': 'application/x-www-form-urlencoded'})   
+    if result.status_code == 200:
+        xmlresponse = result.content
+        dom = parseString(xmlresponse)
+        status = dom.documentElement.getAttribute('status')
+        if status == PARSE_ERROR:
+            details = dom.documentElement.getElementsByTagName('details')[0]
+            msg = details.getAttribute('message')
+            line = int(details.getAttribute('line'))
+            column = int(details.getAttribute('column'))
+            code_line = ''
+            lines = code.splitlines()
+            if lines:
+                code_line = lines[line-1] 
+            return ValidationResult(status, message=msg, line=line, column=column, code_line=code_line)
+        else:
+            codeElement = dom.documentElement.getElementsByTagName('code')[0]
+            residualCode = codeElement.firstChild.data
+            svg = dom.documentElement.getElementsByTagName('tree')[0].firstChild.toxml()
+            return SupercompilationResult(OK, residualCode, svg)
+    else:
+        return ValidationResult(NETWORK_ERROR)
 
 class MenuItem(object):
     def __init__(self, key, text, link=None):
@@ -62,26 +143,15 @@ class SLProgramAdd(webapp.RequestHandler):
     def post(self):
         if not users.get_current_user():
             self.redirect(users.create_login_url(self.request.uri))
-            return
         code = self.request.get('code')
         goal = self.request.get('goal')
-        try:
-            funs = slparsing.parseAndValidate(code)
-        except pyparsing.ParseException, pe:
-            line = pyparsing.line(pe.loc, pe.pstr)
-            msg = str(pe)
-            self.display_errors(code_error=str(pe), code_line=line)
-            return
         
-        found = False
-        for fun in funs:
-            if fun.fFun:
-                if fun.fFun.name == goal:
-                    found = True
-                    break
-        
-        if not found:
+        validationResult = validateProgram(code, goal)
+        if validationResult.status == UNKNOWN_FUNCTION:
             self.display_errors(no_f_function=True)
+            return
+        elif validationResult.status == PARSE_ERROR:
+            self.display_errors(code_error=validationResult.message, code_line=validationResult.code_line)
             return
         
         program = SLProgram()        
@@ -128,24 +198,15 @@ class SLProgramEdit(webapp.RequestHandler):
             self.redirect(users.create_login_url(self.request.uri))
         code = self.request.get('code')
         goal = self.request.get('goal')
-        try:
-            funs = slparsing.parseAndValidate(code)
-        except pyparsing.ParseException, pe:
-            line = pyparsing.line(pe.loc, pe.pstr)
-            msg = str(pe)
-            self.display_errors(code_error=str(pe), code_line=line)
-            return
         
-        found = False
-        for fun in funs:
-            if fun.fFun:
-                if fun.fFun.name == goal:
-                    found = True
-                    break
-        
-        if not found:
+        validationResult = validateProgram(code, goal)
+        if validationResult.status == UNKNOWN_FUNCTION:
             self.display_errors(no_f_function=True)
             return
+        elif validationResult.status == PARSE_ERROR:
+            self.display_errors(code_error=validationResult.message, code_line=validationResult.code_line)
+            return
+        
         try:
             key_name = self.request.get('key')
             slprogram = db.get(db.Key(key_name))
@@ -214,11 +275,39 @@ class SLProgramDelete(webapp.RequestHandler):
         except db.BadKeyError:
             self.redirect('/')
 
+class SLProgramRun(webapp.RequestHandler):
+    def get(self):
+        key_name = self.request.get('key')
+        try:
+            slprogram = db.get(db.Key(key_name))
+            if slprogram:
+                scpResult = supercompileProgram(slprogram.code, slprogram.goal)
+                # print scpResult.residualCode
+                # print scpResult.svgTree
+                navitems = createNavItems("sample_edit")
+                useritems = createUserItems(self.request.uri, '/')
+                template_values = {
+                                   'program': slprogram,
+                                   'scpResult': scpResult,
+                                   'navitems':navitems,
+                                   'useritems':useritems
+                                   }
+
+                path = os.path.join(os.path.dirname(__file__), 'sample_run.xhtml')
+                self.response.out.write(template.render(path, template_values))
+                self.response.headers.add_header('Content-Type', 'text/xml; charset=utf-8')
+                return
+            else:
+                self.redirect('/')
+        except db.BadKeyError:
+            self.redirect('/')
+
 application = webapp.WSGIApplication(
                                      [('/', SLProgramList),
                                       ('/add', SLProgramAdd),
                                       ('/delete', SLProgramDelete),
-                                      ('/edit', SLProgramEdit)]
+                                      ('/edit', SLProgramEdit),
+                                      ('/run', SLProgramRun)]
                                      )
 
 def main():
