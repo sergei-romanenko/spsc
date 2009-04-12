3e37447314e5ed52b820a812b36fefd3e75ea0b8
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2009-04-12 17:17:51 +0000

    one more line

--- spsc_lite_v1/src/spsc/spsc.scala
+++ spsc_lite_v1/src/spsc/spsc.scala
@@ -40,7 +40,7 @@ object Algebra {
     case FCall(n, vs) => FCall(n, vs.map(sub(_, map)))
     case GCall(n, vs) => GCall(n, vs.map(sub(_, map)))
   }
-  def inst(t1: Term, t2: Term): Boolean = findSub(t1, t2) != null
+  def inst(t1: Term, t2: Term) = findSub(t1, t2) != null
   def findSub(t1: Term, t2: Term) = {
     val map = scala.collection.mutable.Map[Var, Term]()
     def walk(t1: Term, t2: Term): Boolean = (t1, t2) match {
@@ -56,11 +56,11 @@ object Algebra {
   }
 }
 
-class Edge(val parent: Node, var child: Node, val pat: Pattern)
+class Edge(val parent: Node, var out: Node, val pat: Pattern)
 case class Node(expr: Term, in: Edge, var outs: List[Edge], var fnode: Node) {
   def ancestors: List[Node] = if (in == null) Nil else in.parent :: in.parent.ancestors
-  def leaves: List[Node] = if (outs.isEmpty) List(this) else List.flatten(children map {_.leaves})
-  def children : List[Node] = outs map {_.child}
+  def leaves: List[Node] = if (outs.isEmpty) List(this) else List.flatten(children.map(_.leaves))
+  def children : List[Node] = outs map {_.out}
   def isProcessed: Boolean = expr match {
     case Ctr(_, Nil) => true
     case v: Var => true
@@ -69,11 +69,11 @@ case class Node(expr: Term, in: Edge, var outs: List[Edge], var fnode: Node) {
 }
 class Tree(var root: Node) {
   def leaves = root.leaves
-  def replace(node: Node, exp: Term) =  node.in.child = Node(exp, node.in, Nil, null)
+  def replace(node: Node, exp: Term) =  node.in.out = Node(exp, node.in, Nil, null)
   def addChildren(node: Node, children: List[(Term, Pattern)]) =
     node.outs = for ((term, p) <- children) yield {
       val edge = new Edge(node, null, p)
-      edge.child = Node(term, edge, Nil, null)
+      edge.out = Node(term, edge, Nil, null)
       edge
     }
 }
@@ -109,6 +109,7 @@ class SuperCompiler(p: Program){
   private def freshPat(p: Pattern) = Pattern(p.name, p.args.map {_ => i += 1; Var("v" + i)})
 }
 class ResidualProgramGenerator(val tree: Tree) {
+  var (sigs, defs) = (Map[Node, (String, List[Var])](),List[Either[FFun, GFun]]())
   lazy val result = (walk(tree.root), Program(defs.toList))
   private def walk(n: Node): Term = if (n.fnode == null) n.expr match {
     case v: Var => v
@@ -116,22 +117,17 @@ class ResidualProgramGenerator(val tree: Tree) {
     case Let(_,bs) => sub(walk(n.children(0)), Map()++bs.map{_._1}.zip(n.children.tail map walk))
     case c: Term =>
       if (n.outs(0).pat != null) {
-        sigs += (n -> (rename(c.name, "g"), vars(c)))
-        for (e <- n.outs) defs = Right(GFun(sigs(n)._1, e.pat, vars(c).tail, walk(e.child))) :: defs
+        sigs += (n -> ("g" + c.name.drop(1) + sigs.size, vars(c)))
+        for (e <- n.outs) defs = Right(GFun(sigs(n)._1, e.pat, vars(c).tail, walk(e.out))) :: defs
         GCall(sigs(n)._1, vars(c))
       } else if (tree.leaves.exists(_.fnode == n)) {
-        sigs += (n -> (rename(c.name, "f"), vars(c)))
+        sigs += (n -> ("f" + c.name.drop(1) + sigs.size, vars(c)))
         defs = Left(FFun(sigs(n)._1, sigs(n)._2, walk(n.children(0)))) :: defs
         FCall(sigs(n)._1, vars(c))
       } else walk(n.children(0))
   } else if (n.fnode.outs(0).pat == null)
-    sub(Function.tupled(FCall)(sigs(n.fnode)), findSub(n.fnode.expr, n.expr))
-  else
-    sub(Function.tupled(GCall)(sigs(n.fnode)), findSub(n.fnode.expr, n.expr))
-
-  var (sigs, defs) = (Map[Node, (String, List[Var])](),List[Either[FFun, GFun]]())
-  var i = 0
-  def rename(f: String, b: String) = {i+=1; b + f.drop(1) + i}
+       sub(Function.tupled(FCall)(sigs(n.fnode)), findSub(n.fnode.expr, n.expr))
+  else sub(Function.tupled(GCall)(sigs(n.fnode)), findSub(n.fnode.expr, n.expr))
 }
 
 import scala.util.parsing.combinator.ImplicitConversions
