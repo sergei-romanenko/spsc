e5da7eb3fc2339f40c55131893ecc30d689256d1
Sergei Romanenko <sergei.romanenko@supercompilers.ru>
2009-05-20 22:56:43 +0000

    A cosmetic refactoring

--- spsc_lite/src/spsc/SLanguage.scala
+++ spsc_lite/src/spsc/SLanguage.scala
@@ -1,22 +1,42 @@
 package spsc
 
-abstract class Term 
+abstract class Term
+
 case class Var(name: String) extends Term {
   override def toString = name
 }
-case class Ctr(name: String, args: List[Term]) extends Term {
+
+abstract class CFGTerm extends Term {
+  def name: String;
+  def args: List[Term];
+  def eqAtTop(e: CFGTerm): Boolean
+  def replaceArgs(newArgs: List[Term]): CFGTerm
+}
+
+case class Ctr(name: String, args: List[Term]) extends CFGTerm {
+  override def eqAtTop(e:CFGTerm) = e.isInstanceOf[Ctr] && name == e.name
+  override def replaceArgs(newArgs: List[Term]) : Ctr = Ctr(name, newArgs)
   override def toString = name + args.mkString("(", ", " ,")")
 }
-abstract class Call extends Term {def name: String}
+
+abstract class Call extends CFGTerm
+
 case class FCall(name: String, args: List[Term]) extends Call {
+  override def eqAtTop(e:CFGTerm) = e.isInstanceOf[FCall] && name == e.name
+  override def replaceArgs(newArgs: List[Term]) : FCall = FCall(name, newArgs)
   override def toString = name + args.mkString("(", ", " ,")")
 }
+
 case class GCall(name: String, args: List[Term]) extends Call {
+  override def eqAtTop(e:CFGTerm) = e.isInstanceOf[GCall] && name == e.name
+  override def replaceArgs(newArgs: List[Term]) : GCall = GCall(name, newArgs)
   override def toString = name + args.mkString("(", ", " ,")")
 }
+
 case class Let(term: Term, bindings: List[(Var, Term)]) extends Term {
   override def toString = "let " + bindings.map{case (v, t) => v + "=" + t}.mkString(", ") + " in " + term
 }
+
 case class Pattern(name: String, args: List[Var]) {
   override def toString = name + args.mkString("(", ", " ,")")
 }
@@ -25,6 +45,7 @@ abstract class Def {def name: String}
 case class FFun(name: String, args: List[Var], term: Term) extends Def {
   override def toString = name + args.mkString("(", ", " ,")") + " = " + term + ";"
 }
+
 case class GFun(name: String, p: Pattern, args: List[Var], term: Term) extends Def {
   override def toString = name + (p :: args).mkString("(", ", " ,")")  + " = " + term + ";"
 }
