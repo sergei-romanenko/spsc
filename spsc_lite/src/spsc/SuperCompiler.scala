93202d11c6cbd1ac859b105603de582d48db1f0f
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2009-05-22 07:03:42 +0000

    formatting for paper

--- spsc_lite/src/spsc/SuperCompiler.scala
+++ spsc_lite/src/spsc/SuperCompiler.scala
@@ -4,20 +4,18 @@ import Algebra._
 
 class SuperCompiler(p: Program){
   
-  val debug = false
-  
   def driveExp(expr: Term): List[(Term, Contraction)] = expr match {
     case Ctr(name, args) => args.map((_,null))
     case FCall(name, args)  =>
       List((subst(p.f(name).term, Map(p.f(name).args.zip(args): _*)), null))
     case GCall(name, Ctr(cname, cargs) :: args) =>
       val g = p.g(name, cname)  
-      List((subst(g.term, Map((g.p.args:::g.args) zip (cargs ::: args): _*)), null))
+      List((subst(g.term, Map((g.p.args:::g.args) zip (cargs:::args): _*)), null))
     case gCall @ GCall(name, (v : Var) :: args) => 
-      for (g <- p.gs(name); val pat = freshPat(g.p); val cons = Ctr(pat.name, pat.args))
-        yield (driveExp(subst(gCall, Map(v -> cons)))(0)._1, Contraction(v, pat))
-    case GCall(name, args) => driveExp(args(0)) map {p =>
-      (GCall(name, p._1 :: args.tail), p._2)}
+      for (g <- p.gs(name); fp = freshPat(g.p); cons = Ctr(fp.name, fp.args))
+        yield (driveExp(subst(gCall, Map(v -> cons)))(0)._1, Contraction(v, fp))
+    case GCall(name, args) => 
+      driveExp(args(0)) map {p => (GCall(name, p._1 :: args.tail), p._2)}
     case Let(term, bs) =>
       (term, null) :: bs.map {pair => (pair._2, null)}
   }
@@ -26,7 +24,6 @@ class SuperCompiler(p: Program){
     val n = new Node(e, null, null)
     var t = new Tree(n, Map().withDefaultValue(Nil))
     while (t.leaves.exists{!_.isProcessed}) {
-      if (debug) {println(t); println()}
       val b = t.leaves.find(!_.isProcessed).get
       t = if (trivial(b.expr)) {
         t.addChildren(b, driveExp(b.expr)) //drive
@@ -40,15 +37,14 @@ class SuperCompiler(p: Program){
           case None => t.addChildren(b, driveExp(b.expr)) // drive
         }
       }
-    }   
-    if (debug) {println(t); println()}
+    }
     t
   }
   
   def abs(t: Tree, a: Node, b: Node) =
     ((g: Gen) => t.replace(a, Let(g.t, g.m1.toList))) (msg(a.expr, b.expr))
   
-  private def freshPat(p: Pattern) = Pattern(p.name, p.args map freshVar)
+  private def freshPat(p: Pat) = Pat(p.name, p.args map freshVar)
   
   def split(t: Tree, n: Node) : Tree = n.expr match {
     case e : CFG =>
