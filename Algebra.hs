b6cf98f0649792c11b30911e0409b6c1f6fc0727
Sergei Romanenko <sergei.romanenko@supercompilers.ru>
2009-08-05 13:58:51 +0000

    Algebra

--- Algebra.hs
+++ Algebra.hs
@@ -1,13 +1,19 @@
 module Algebra where
 
 import qualified Data.Map as M
+import qualified Data.List as List
 import Control.Monad.State
 
+
 import SLanguage
+
+theSameFunctor :: Exp -> Exp -> Bool
 
 theSameFunctor (Call kind1 name1 _) (Call kind2 name2 _) =
   kind1 == kind2 && name1 == name2
 theSameFunctor _ _ = False
+
+substExp :: M.Map Name Exp -> Exp -> Exp
 
 substExp m e =
   case e of
@@ -16,40 +22,54 @@ substExp m e =
     Call kind name args ->
       Call kind name (map (substExp m) args)
 
---  
---  def equiv(t1: Term, t2: Term): Boolean = inst(t1, t2) && inst(t2, t1)
+equiv :: Exp -> Exp -> Bool
 
-equiv exp1 exp2 = True
+equiv e1 e2 = e1 `instOf` e2 && e2 `instOf` e1 
 
---  
---  def inst(t1: Term, t2: Term): Boolean = findSubst(t1, t2) != null
---  
---  def findSubst(t1: Term, t2: Term): Map[Var, Term] = {
---    val map = scala.collection.mutable.Map[Var, Term]()
---    def walk(t1: Term, t2: Term): Boolean = (t1, t2) match {
---      case (v1: Var, _) => map.put(v1, t2) match {case None => true; case Some(t3) => t2 == t3}
---      case (e1: CFG, e2:CFG) if shellEq(e1, e2) => List.forall2(e1.args, e2.args)(walk)
---      case _ => false
---    }
---    if (walk(t1, t2)) Map(map.toList:_*).filter{case (k, v) => k != v} else null
---  }
---  
---  def vars(t: Term): List[Var] = t match {
---    case v: Var => (List(v))
---    case e: CFG => (List[Var]() /: e.args) {_ union vars(_)}
---  }
---  
---  def freshVar(x: AnyRef) = {i += 1; Var("v" + i)}; private var i = 0;
---  
---  def trivial(expr: Term): Boolean = expr match {
---    case FCall(_, _) => false
---    case GCall(_, _) => false
---    case _ => true
---  }
+instOf :: Exp -> Exp -> Bool
+
+instOf e' e =
+  case matchAgainst e e' of
+    Nothing -> False
+    Just _ -> True
+
+matchAgainst :: Exp -> Exp -> Maybe (M.Map Name Exp)
+
+matchAgainst e e' = matchAgainstAcc (Just M.empty) e e'
+
+matchAgainstAcc :: Maybe (M.Map Name Exp) -> Exp -> Exp ->
+                   Maybe (M.Map Name Exp)
+
+matchAgainstAcc (Just m) (Var vname) e' =
+  case M.lookup vname m of
+    Nothing -> Just $ M.insert vname e' m
+    Just e'' ->
+      if e' /= e'' then Nothing else Just m
+matchAgainstAcc (Just m) (Call kind name args) (Call kind' name' args')
+  | kind == kind' && name == name' =
+        matchAgainstAccL (Just m) args args'
+matchAgainstAcc _ _ _ = Nothing
+
+matchAgainstAccL :: Maybe (M.Map Name Exp) -> Args -> Args ->
+                    Maybe (M.Map Name Exp)
+
+matchAgainstAccL (Just m) [] [] = (Just m)
+matchAgainstAccL (Just m) (e : es) (e' : es') =
+  matchAgainstAccL (matchAgainstAcc (Just m) e e') es es'
+matchAgainstAccL _ _ _ = Nothing
+
+vars :: Exp -> [Name]
+vars (Var vname) = [vname]
+vars (Call _ _ args) =
+  foldl List.union [] (map vars args)
+
+isFGCall :: Exp -> Bool
 
 isFGCall (Call FCall _ _) = True
 isFGCall (Call GCall _ _) = True
 isFGCall _ = False 
+
+mkName :: (Show a) => a -> [Char]
 
 mkName t = "$" ++ show t
 
@@ -58,6 +78,8 @@ freshName =
   do t <- get
      put $ t+1
      return $ mkName t
+
+freshNameList :: (MonadState a m, Num a, Enum a) => a -> m [String]
 
 freshNameList n =
   do t <- get
