a198697926281938e252bfc39cce48c53e4b1bf8
Sergei Romanenko <sergei.romanenko@supercompilers.ru>
2021-11-11 23:09:06 +0300

    spsc-lite-julia: Cosmetic changes.

--- spsc-lite-julia/test/advanced_process_tree_builder_tests.jl
+++ spsc-lite-julia/test/advanced_process_tree_builder_tests.jl
@@ -34,23 +34,36 @@ const pAddAcc = "gAddAcc(Z,y)=y;gAddAcc(S(x),y)=gAddAcc(x,S(y));"
         "{0:(S(Z),nothing,nothing,[1]),1:(Z,nothing,0,[])}")
     buildPrTree1OK(pAddAcc, "gAddAcc(S(Z), Z)",
         "{0:(gAddAcc(S(Z),Z),nothing,nothing,[1]),1:(gAddAcc(Z,S(Z)),nothing,0,[])}")
-    buildPrTree1OK(pAdd, "gAdd(a, b)",
-        "{0:(gAdd(a,b),nothing,nothing,[1,2]),1:(b,a=Z,0,[]),2:(S(gAdd(v100,b)),a=S(v100),0,[])}")
-    buildPrTree1OK(pAdd, "gAdd(gAdd(a,b),c)",
-        "{0:(gAdd(gAdd(a,b),c),nothing,nothing,[1,2]),1:(gAdd(b,c),a=Z,0,[]),2:(gAdd(S(gAdd(v100,b)),c),a=S(v100),0,[])}")
+    buildPrTree1OK(pAdd, "gAdd(a, b)", "{" *
+        "0:(gAdd(a,b),nothing,nothing,[1,2]),1:(b,a=Z,0,[])," *
+        "2:(S(gAdd(v100,b)),a=S(v100),0,[])}")
+    buildPrTree1OK(pAdd, "gAdd(gAdd(a,b),c)", "{" *
+        "0:(gAdd(gAdd(a,b),c),nothing,nothing,[1,2]),1:(gAdd(b,c),a=Z,0,[])," *
+        "2:(gAdd(S(gAdd(v100,b)),c),a=S(v100),0,[])}")
 end
     
 @testset "buildPrTree" begin
-    buildPrTreeOK(pAddAcc, "gAddAcc(a,b)",
-        "{0:(gAddAcc(a,b),nothing,nothing,[1,2]),1:(b,a=Z,0,[]),2:(let a=v100,b=S(b) in gAddAcc(a,b),a=S(v100),0,[3,4,5]),3:(gAddAcc(a,b),nothing,2,[]),4:(v100,nothing,2,[]),5:(S(b),nothing,2,[6]),6:(b,nothing,5,[])}")
-    buildPrTreeOK(pAddAcc, "gAddAcc(S(Z), Z)",
-        "{0:(gAddAcc(S(Z),Z),nothing,nothing,[1]),1:(gAddAcc(Z,S(Z)),nothing,0,[2]),2:(S(Z),nothing,1,[3]),3:(Z,nothing,2,[])}")
-    buildPrTreeOK(pAdd, "gAdd(a, b)",
-        "{0:(gAdd(a,b),nothing,nothing,[1,2]),1:(b,a=Z,0,[]),2:(S(gAdd(v100,b)),a=S(v100),0,[3]),3:(gAdd(v100,b),nothing,2,[])}")
-    buildPrTreeOK(pAdd, "gAdd(gAdd(a,b),c)",
-        "{0:(gAdd(gAdd(a,b),c),nothing,nothing,[1,2]),1:(gAdd(b,c),a=Z,0,[3,4]),3:(c,b=Z,1,[]),4:(S(gAdd(v101,c)),b=S(v101),1,[5]),5:(gAdd(v101,c),nothing,4,[]),2:(gAdd(S(gAdd(v100,b)),c),a=S(v100),0,[6]),6:(S(gAdd(gAdd(v100,b),c)),nothing,2,[7]),7:(gAdd(gAdd(v100,b),c),nothing,6,[])}")
-    buildPrTreeOK(pAdd, "gAdd(a, a)",
-        "{0:(let v102=a,v103=a in gAdd(v102,v103),nothing,nothing,[4,5,6]),4:(gAdd(v102,v103),nothing,0,[7,8]),7:(v103,v102=Z,4,[]),8:(S(gAdd(v104,v103)),v102=S(v104),4,[9]),9:(gAdd(v104,v103),nothing,8,[]),5:(a,nothing,0,[]),6:(a,nothing,0,[])}")
+    buildPrTreeOK(pAddAcc, "gAddAcc(a,b)", "{" *
+        "0:(gAddAcc(a,b),nothing,nothing,[1,2]),1:(b,a=Z,0,[])," *
+        "2:(let a=v100,b=S(b) in gAddAcc(a,b),a=S(v100),0,[3,4,5])," *
+        "3:(gAddAcc(a,b),nothing,2,[]),4:(v100,nothing,2,[])," *
+        "5:(S(b),nothing,2,[6]),6:(b,nothing,5,[])}")
+    buildPrTreeOK(pAddAcc, "gAddAcc(S(Z), Z)", "{" *
+        "0:(gAddAcc(S(Z),Z),nothing,nothing,[1]),1:(gAddAcc(Z,S(Z)),nothing,0,[2])," *
+        "2:(S(Z),nothing,1,[3]),3:(Z,nothing,2,[])}")
+    buildPrTreeOK(pAdd, "gAdd(a, b)", "{" *
+        "0:(gAdd(a,b),nothing,nothing,[1,2]),1:(b,a=Z,0,[])," *
+        "2:(S(gAdd(v100,b)),a=S(v100),0,[3]),3:(gAdd(v100,b),nothing,2,[])}")
+    buildPrTreeOK(pAdd, "gAdd(gAdd(a,b),c)", "{" *
+        "0:(gAdd(gAdd(a,b),c),nothing,nothing,[1,2]),1:(gAdd(b,c),a=Z,0,[3,4])," *
+        "3:(c,b=Z,1,[]),4:(S(gAdd(v101,c)),b=S(v101),1,[5])," *
+        "5:(gAdd(v101,c),nothing,4,[]),2:(gAdd(S(gAdd(v100,b)),c),a=S(v100),0,[6])," *
+        "6:(S(gAdd(gAdd(v100,b),c)),nothing,2,[7]),7:(gAdd(gAdd(v100,b),c),nothing,6,[])}")
+    buildPrTreeOK(pAdd, "gAdd(a, a)", "{" *
+        "0:(let v102=a,v103=a in gAdd(v102,v103),nothing,nothing,[4,5,6])," *
+        "4:(gAdd(v102,v103),nothing,0,[7,8]),7:(v103,v102=Z,4,[])," *
+        "8:(S(gAdd(v104,v103)),v102=S(v104),4,[9]),9:(gAdd(v104,v103),nothing,8,[])," *
+        "5:(a,nothing,0,[]),6:(a,nothing,0,[])}")
 end
 
 end
